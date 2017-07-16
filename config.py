0500de81ae4a99fa0c73f05d09cc0f8c21fe9ab5
valenzione <snyderino@gmail.com>
2017-07-16 15:08:45 +0300

    config update

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 token = '237044631:AAHpc82QrCa0s9_svHcFsqDZV87VsdSc5Dk'
 db_uri = 'mongodb://heroku_76pgrdhp:ud0vc4uvbncdqbtnkptrn74h9i@ds057806.mlab.com:57806/heroku_76pgrdhp'
 
-WEBHOOK_HOST = '188.130.155.151'
+WEBHOOK_HOST = '46.101.182.19'
 WEBHOOK_PORT = 443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
 WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
 
